8e7b2ddda176a195d1bae5a5819c1e4991d09b5e
JAAL1412 <javier.aal1412@gmail.com>
2024-06-16 22:47:48 -0400

    solucion de bugs

--- resources/views/busqueda.blade.php
+++ resources/views/busqueda.blade.php
@@ -54,7 +54,17 @@
       <th scope="col" style="border-left: transparent !important;" class="t">Id</th>
       <th scope="col" class="t">Ingreso</th>
       <th scope="col" class="t">Salida</th>
-      <th scope="col" class="t">Descripción</th>
+      <th scope="col" class="t">Reparacion</th>
+      <th scope="col" class="t">monto</th>
+      <th scope="col" class="t">Comentario</th>
+      <th scope="col" class="t">Acción</th>
+      <td class="t">
+      <form action="{{route('vehiculo.edit', $item->vid)}}" method="GET">
+			<button class="btn btn-warning btn-sm">
+      <ion-icon name="create-outline"></ion-icon>
+			</button>
+	  </form>
+      </td>
     </tr>
   </thead>
  
@@ -64,37 +74,9 @@
       <td style="border-left: transparent !important;" class="t">{{$item1->id}}</td>
       <td class="t">{{$item1->ingreso}} </td>
       <td class="t">{{$item1->salida}}</td>
-      <td class="t">
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal">
-        <ion-icon name="alert-outline"></ion-icon>
-        </button>
-          <div class="modal fade" id="Modal" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-body">
-                <button type="button" class="btn-close" data-bs-dismiss="modal" style="text-align: right;"></button>
-                  <table class="table">
-                    <thead>
-                      <tr>
-                        <th>Reparacion</th>
-                        <th>monto</th>
-                        <th>comentario</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                      <td>{{$item1->reparacion}}</td>
-                      <td>{{$item1->monto}}$</td>
-                      <td>{{$item1->comentario}}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <button type="button" class="btn"> <ion-icon name="create-outline"></ion-icon></button>
-                </div>
-              </div>
-            </div>
-          </div>
-</td>
+      <td class="t">{{$item1->reparacion}}</td>
+      <td class="t">{{$item1->monto}}</td>
+      <td class="t">{{$item1->comentario}}</td>
     </tr>
   @endforeach
   </tbody>
